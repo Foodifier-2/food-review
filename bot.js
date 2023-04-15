60f2090706d32495c1c86fed38c38d83e44aa4f5
dakshshah03 <daksh.shah15@gmail.com>
2023-04-15 10:07:05 -0700

    e

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
-require("dotenv").config(); //to start process from .env file
-const { TOKEN }
-const {Client, Intents}=require("discord.js");
-const client=new Client({
-    Intents:[
-        Intents.FLAGS.GUILDS,//adds server functionality
-        Intents.FLAGS.GUILD_MESSAGES //gets messages from our bot.
-    ]
+require('dotenv').config(); //initialize dotenv
+const Discord = require('discord.js'); //import discord.js
+
+const client = new Discord.Client(); //create new client
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-client.once("ready", () =>{
-    console.log("BOT IS ONLINE"); //message when bot is online
-})
-client.login(process.env.TOKEN);
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN); //login bot using token
