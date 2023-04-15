c6b1ce0c1cb72400b29c706042285ad35e94c13f
dakshshah03 <daksh.shah15@gmail.com>
2023-04-15 16:50:15 -0700

    updated main

---
+++ data_read_write.js
@@ -0,0 +1,32 @@
+const csv = require('csv-parser');
+const { createObjectCsvWriter } = require('csv-writer');
+
+function readData() {
+	return new Promise((resolve, reject) => {
+	  const results = [];
+	  fs.createReadStream('data.csv')
+		.pipe(csv())
+		.on('data', (data) => results.push(data))
+		.on('end', () => resolve(results))
+		.on('error', reject);
+	});
+}
+
+function writeData(data) {
+  const csvWriter = createObjectCsvWriter({
+    path: 'data.csv',
+    header: [
+      { id: 'name', title: 'name' },
+      { id: 'rating', title: 'rating' },
+      { id: 'review', title: 'review' },
+    ],
+  });
+  return csvWriter.writeRecords(data);
+}
+
+async function getAllData() {
+	const data = await readData();
+	return data;
+}
+
+getAllData().then((data) => console.log(data));
