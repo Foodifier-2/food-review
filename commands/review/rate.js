c66e8ebe432ed2602e096581d1d0c61fd51900a4
Jay Tripathi <jaytripathi100@gmail.com>
2023-04-15 09:18:03 -0700

    rate command parameters working

--- commands/review/rate.js
+++ commands/review/rate.js
@@ -6,14 +6,26 @@ module.exports = {
         .setDescription('Add a numerical rating of a UCSC food item and optionally a text review')
         .addStringOption(option =>
             option.setName('food_item')
-            .setRequired('The UCSC food item to review/rate')
+            .setDescription('The UCSC food item to rate')
+            .setAutocomplete(true)
             .setRequired(true))
-        .addStringOption(option =>
-            option.setName('rating')  // Add better name later
+        .addIntegerOption(option =>
+            option.setName('rating')
             .setDescription('A numerical rating from 1 to 10')
-            .setRequired(true))
+            .setRequired(true)
+            .addChoices(
+                { name: '1', value: 1 },
+                { name: '2', value: 2 },
+                { name: '3', value: 3 },
+                { name: '4', value: 4 },
+                { name: '5', value: 5 },
+                { name: '6', value: 6 },
+                { name: '7', value: 7 },
+                { name: '8', value: 8 },
+                { name: '9', value: 9 },
+                { name: '10', value: 10 }))
         .addStringOption(option =>
-            option.setName('review') // Add better name later
+            option.setName('review')
             .setDescription('An optional textual review of the food')
             .setRequired(false)),
             
