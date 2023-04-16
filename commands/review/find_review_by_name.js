31dc54d6187da5e7e757de8146a3c371092d893f
Nat <nmarks413@gmail.com>
2023-04-16 05:40:48 -0700

    jjjjjjj

--- commands/review/find_review_by_name.js
+++ commands/review/find_review_by_name.js
@@ -47,30 +47,50 @@ module.exports = {
         .setAuthor({name: user.username + "#" + user.discriminator})
         .setTitle("Reviews")
         .setDescription(`Food:${reviews[current_page].food_item}\nRating:${reviews[current_page].rating}\nReviews:${reviews[current_page].review}`);
-			const response = await interaction.reply({ embeds: [embed], components: [row]});
+			const response = await interaction.reply({ embeds: [embed], components: [row]})
+      // .then(() => console.log("reply sent."));
 
 
       const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
 
       collector.on('collect', async i => {
-        
-        if(i.customId.localeCompare("right")){
-          console.log("test")
+
+        if(i.customId.localeCompare("right") == 0){
           current_page++;
-          const new_embed = EmbedBuilder.from(embed).setDescription(`Food:${reviews[current_page].food_item}\nRating:${reviews[current_page].rating}\nReviews:${reviews[current_page].review}`);
+        } else{
+          current_page--;
+        }
+          if(current_page == reviews.length) {
+              const right_new = new ButtonBuilder()
+                .setLabel("->")
+                .setCustomId("right")
+                .setStyle(ButtonStyle.Primary)
+                .setDisabled(true)
+          }
+
+          if(current_page != 0){
+            const left_new = new ButtonBuilder()
+              .setLabel("<-")
+              .setCustomId("left")
+              .setStyle(ButtonStyle.Primary)
+              .setDisabled(false)
+          }
+
+          const row_new = new ActionRowBuilder()
+            .addComponents(left_new, right_new)
+
+          const new_embed = new EmbedBuilder()
+          .setAuthor({name: user.username + "#" + user.discriminator})
+          .setTitle("Reviews")
+          .setDescription(`Food:${reviews[current_page].food_item}\nRating:${reviews[current_page].rating}\nReviews:${reviews[current_page].review}`);
 
           const channel = client.channels.cache.get(response.interaction.channelId);
 
           const message = await channel.messages.fetch(response.interaction.messageId);
 
-          console.log(message)
+          // console.log(message)
 
-
-          message.edit({embeds: [new_embed], componets: [row]});
-        } else{
-
-        }
-
+          response.edit({embeds: [new_embed], componets: [row_new]});
       });
 
     } else{
